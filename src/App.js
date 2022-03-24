dc6c9729915be7199ba0df0e66198f32376c5003
sadikhossain69 <sadikhossain433@gmail.com>
2022-03-25 01:19:09 +0600

    Navbar optimized

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-center'>Hello World!</h1>
+    <>
+      <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
+      </div>
+    </>
   );
 }
 
